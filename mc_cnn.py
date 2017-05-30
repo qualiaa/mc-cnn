d2b8ca9fa688c5a3a413af28c58c02e06ca3ca4d
Jamie Bayne <jamiebayne@hotmail.com>
2017-05-30 20:38:56 +0100

    Finish base network

--- mc_cnn.py
+++ mc_cnn.py
@@ -4,6 +4,13 @@ import tensorflow as tf
 
 import mc_cnn_input as examples
 
+def _layer_summary(name,h,w,b):
+    tf.summary.histogram('biases', b)
+    tf.summary.histogram('weights', b)
+    tf.summary.histogram('activations/hist', h)
+    tf.summary.scalar('activations/sparsity',
+                      tf.nn.zero_fraction(h))
+
 def _bias_variable(size,name="bias"):
     init = tf.zeros([size])
     return tf.Variable(init,name=name);
@@ -18,39 +25,48 @@ def _conv_layer(input_,shape,name="conv",activation_fn=tf.nn.relu):
         w = _weight_variable(shape)
 
         wx = tf.nn.conv2d(input_,w,strides=[1,1,1,1],padding="VALID")
-        z = tf.add(wx,b,axis=2)
-        activations = activation_fn(z, name=scope.name)
+        z = tf.add(wx,b)
+        activations = activation_fn(z, name=scope)
+        _layer_summary(scope,activations,w,b)
     return activations
 
-def _fc_layer(input_,size,name="fc",activation_fn=tf.nn.relu):
+def _fc_layer(input_,input_size,output_size,name="fc",activation_fn=tf.nn.relu):
     with tf.name_scope(name) as scope:
-        weight_shape = tf.stack(tf.size(input_),size)
-        b = _bias_variable(size)
+        weight_shape = tf.stack([input_size,output_size])
+        b = _bias_variable(output_size)
         w = _weight_variable(weight_shape)
 
-        wx = tf.matmul(w,input_)
+        wx = tf.matmul(input_, w)
         z = tf.add(wx,b)
-        activations = activation_fn(z, name=scope.name)
+        activations = activation_fn(z, name=scope)
+        _layer_summary(scope,activations,w,b)
     return activations
+
+def _flatten(input_):
+    return tf.reshape(input_,[tf.shape(input_)[0],-1], name="flatten")
 
 
 def inference(left, right, channels=1):
     
     def conv_layers(input_):
         with tf.name_scope("conv_layers"):
-            conv1 = _conv_layer(input_,[1,5,5,channels])
-            fc1 = _fc_layer(conv1,200)
-            fc2 = _fc_layer(fc1,200)
+            conv1 = _conv_layer(input_,[5,5,channels,32])
+            fc1 = _fc_layer(_flatten(conv1),32*5*5,200)
+            fc2 = _fc_layer(fc1,200,200)
 
-
-        return fc2
+        return fc1
             
     left = conv_layers(left)
     right = conv_layers(right)
 
-    concat = tf.concat([left, right],axis=0)
+    concat = tf.concat([left, right],axis=1)
+
+    fc3 = _fc_layer(concat,400,300)
+    fc4 = _fc_layer(fc3,300,300)
+    fc5 = _fc_layer(fc4,300,300)
+    fc6 = _fc_layer(fc5,300,300)
     
-    softmax = _fc_layer(concat,2,name="softmax",activation_fn=tf.nn.softmax)
+    softmax = _fc_layer(fc6,300,2,name="softmax",activation_fn=tf.nn.softmax)
 
     return softmax
 
@@ -68,7 +84,7 @@ if __name__ == "__main__":
         threads = tf.train.start_queue_runners(coord=coord)
 
         sess.run(op)
-        #summarize(tf.summary.merge_all())
+        summarize(tf.summary.merge_all())
 
         coord.request_stop()
         coord.join(threads)
